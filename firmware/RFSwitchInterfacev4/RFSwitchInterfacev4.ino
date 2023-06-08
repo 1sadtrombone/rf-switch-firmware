c0a9df7fdb5e3e0876ac8e45f2751a7ca2ba5ac2
Taj Dyson <taj.dyson@gmail.com>
2023-06-08 11:48:40 -0700

    implemented delay()-less pulsing and the v2 commands (except all, which would be complicated)

--- firmware/RFSwitchInterfacev4/RFSwitchInterfacev4.ino
+++ firmware/RFSwitchInterfacev4/RFSwitchInterfacev4.ino
@@ -11,6 +11,8 @@
 
 int i,j;
 int nSleep = A3;
+int Vout_read_pin = A6;
+int Vout_set_pin = 11;
 int iSense = A7;
 int nFaultPins[] = {2,3,4,5,6,7,8,9,10}; // try 2, .. 10
 byte chipAddr[] = {0x61,0x62,0x63,0x64,0x65,0x66,0x67,0x68,0x69};
@@ -30,7 +32,7 @@ float Gain = 50; // From datasheet
 float currentScale = V_operating/(Rshunt * Gain)/1024.0*1000; // mA
 int zeroCurrentLevel = 8; // what analogRead reads with no current
 
-float Vout_set = 3.2; // can't go lower than 1.1 V, chips need 2.6 V anyway
+float Vout_set = 3.2; // can't go lower than 1.1 V, chips need 2.7 V anyway
 int Vout_set_int;
 
 int writeError;
@@ -40,8 +42,8 @@ String instring = "dur"; // print the pulse durations on boot
 bool messageDone = true;
 bool streamOn = false;
 
-int onPulseDuration = 20; // ms. Amount of time the switching current is on to open the switch
-int offPulseDuration = 70; // ms. Ditto to close the switch
+int opnPulseDuration = 20; // ms. Amount of time the switching current is on to open the switch
+int clsPulseDuration = 70; // ms. Ditto to close the switch
 
 int switch_index;
 int port_index;
@@ -49,9 +51,9 @@ int port_index;
 int tick_count;
 int ticks_per_read = 500;
 
-unsigned long t1;
-unsigned long t2; // for timing the pulses without delay()
+unsigned long pulse_start; // for timing the pulses without delay()
 int activeCnum = -1; // chip currently pulsing
+int pulseDuration;
 
 void writeRegister(byte addr, byte reg, byte val){
   Wire.beginTransmission(addr);
@@ -93,11 +95,11 @@ void initializeChips(){
 bool onOutput(int chipNum, int pinNum) {
   commandBits = 0b00000100;
   bitSet(commandBits, pinNum+3); // bits 3 to 7 set outputs 0 to 3.
-  writeRegister(chipAddr[chipNum],IC_1,commandBits);
+  writeRegister(chipAddr[chipNum],I2C_1,commandBits);
 }
 
 bool offOutput(int chipNum){
-  writeRegister(chipAddr[chipNum],IC_1,0b00000100);
+  writeRegister(chipAddr[chipNum],I2C_1,0b00000100);
 }
 
 float getCurrent(){
@@ -119,86 +121,107 @@ void setVoltage(float Vout_set){
 
 void messageParse(String message){
   // first 3 chars are the command
-  if (message.substring(0,3) == "dur") { // short for duration
-  // print the pulse durations
-  // syntax: "dur"
-  Serial.print("Closing pulse duration is ");
-  Serial.print(offPulseDuration);
-  Serial.println(" ms");
-  Serial.print("Opening pulse duration is ");
-  Serial.print(onPulseDuration);
-  Serial.println(" ms");
+  if (activeCnum < 0) {
+    if (message.substring(0,3) == "dur") { // short for duration
+    // print the pulse durations
+    // syntax: "dur"
+    Serial.print("Closing pulse duration is ");
+    Serial.print(clsPulseDuration);
+    Serial.println(" ms");
+    Serial.print("Opening pulse duration is ");
+    Serial.print(opnPulseDuration);
+    Serial.println(" ms");
 
-  } else if (message.substring(0,3) == "opd") { // "open duration"
-  // set the opening pulse duration
-  // syntax: "opd [integer duration in ms]"
-    if (isDigit(message.charAt(4))) {
-      onPulseDuration = message.substring(4).toInt();
-      Serial.print("Set open-toggling pulse duration to ");
-      Serial.print(onPulseDuration);
-      Serial.println(" ms");
-    } else {
-      Serial.println("! Duration specified is NaN");
-    }
+    } else if (message.substring(0,3) == "opd") { // "open duration"
+    // set the opening pulse duration
+    // syntax: "opd [integer duration in ms]"
+      if (isDigit(message.charAt(4))) {
+        opnPulseDuration = message.substring(4).toInt();
+        Serial.print("Set open-toggling pulse duration to ");
+        Serial.print(opnPulseDuration);
+        Serial.println(" ms");
+      } else {
+        Serial.println("! Duration specified is NaN");
+      }
 
-  } else if (message.substring(0,3) == "cld") { // "close duration"
-  // set the closing pulse duration
-  // syntax: "cld [integer duration in ms]"
-    if (isDigit(message.charAt(4))) {
-      offPulseDuration = message.substring(4).toInt();
-      Serial.print("Set closed-toggling pulse duration to ");
-      Serial.print(offPulseDuration);
-      Serial.println(" ms");
-    } else {
-      Serial.println("! Duration specified is NaN");
-    }
+    } else if (message.substring(0,3) == "cld") { // "close duration"
+    // set the closing pulse duration
+    // syntax: "cld [integer duration in ms]"
+      if (isDigit(message.charAt(4))) {
+        clsPulseDuration = message.substring(4).toInt();
+        Serial.print("Set closed-toggling pulse duration to ");
+        Serial.print(clsPulseDuration);
+        Serial.println(" ms");
+      } else {
+        Serial.println("! Duration specified is NaN");
+      }
 
-  } else if (message.substring(0,3) == "tog") { // toggle
-  // turn switch port open or closed
-  // syntax: "tog [integer switch #] [integer port #] ['opn' / 'cls']"
-  // switch and port number are 1-indexed in the command
-    if (isDigit(message.charAt(4))) {
-      if (message.substring(4).toInt() > 0 && message.substring(4).toInt() < 4) {
-        switch_index =  message.substring(4).toInt() - 1;
-        if (isDigit(message.charAt(6))) {
-            if (message.substring(6).toInt() > 0 && message.substring(6).toInt() < 7) {
-              port_index = message.substring(6).toInt() - 1;
-              if (message.substring(8) == "opn") {
-                onOutput(Cnums[switch_index][port_index], opnPnums[switch_index][port_index]);
-                Serial.print("Toggling open, chip ");
-                Serial.print(Cnums[switch_index][port_index]);
-                Serial.print(" pin ");
-                Serial.println(opnPnums[switch_index][port_index]);
-                delay(onPulseDuration);
-                offOutput(Cnums[switch_index][port_index]);
-                Serial.println("Done.");
-              } else if (message.substring(8) == "cls") {
-                onOutput(Cnums[switch_index][port_index], clsPnums[switch_index][port_index]);
-                Serial.print("Toggling closed, chip ");
-                Serial.print(Cnums[switch_index][port_index]);
-                Serial.print(" pin ");
-                Serial.println(clsPnums[switch_index][port_index]);
-                delay(offPulseDuration);
-                offOutput(Cnums[switch_index][port_index]);
-                Serial.println("Done.");
+    } else if (message.substring(0,3) == "vlt") { // short for voltage
+    // set the arduino output voltage
+    // syntax: "vlt [float voltage in V]"
+      if (isDigit(message.charAt(4))) {
+        Vout_set = message.substring(4).toFloat();
+        setVoltage(Vout_set);
+        Serial.print("Set voltage out to ");
+        Serial.print(Vout_set);
+        Serial.println(" ms");
+      } else {
+        Serial.println("! Voltage specified is NaN");
+      }
+
+    } else if (message.substring(0,3) == "stm") { // for "stream"
+      // toggles printing the voltage and current every cycle
+      streamOn = !streamOn;
+
+    } else if (message.substring(0,3) == "tog") { // toggle
+    // turn switch port open or closed
+    // syntax: "tog [integer switch #] [integer port #] ['opn' / 'cls']"
+    // switch and port number are 1-indexed in the command
+      if (isDigit(message.charAt(4))) {
+        if (message.substring(4).toInt() > 0 && message.substring(4).toInt() < 4) {
+          switch_index =  message.substring(4).toInt() - 1;
+          if (isDigit(message.charAt(6))) {
+              if (message.substring(6).toInt() > 0 && message.substring(6).toInt() < 7) {
+                port_index = message.substring(6).toInt() - 1;
+                if (message.substring(8) == "opn") {
+                  onOutput(Cnums[switch_index][port_index], opnPnums[switch_index][port_index]);
+                  Serial.print("Toggling open, chip ");
+                  Serial.print(Cnums[switch_index][port_index]);
+                  Serial.print(" pin ");
+                  Serial.println(opnPnums[switch_index][port_index]);
+                  activeCnum = Cnums[switch_index][port_index];
+                  pulse_start = millis();
+                  pulseDuration = opnPulseDuration;
+                } else if (message.substring(8) == "cls") {
+                  onOutput(Cnums[switch_index][port_index], clsPnums[switch_index][port_index]);
+                  Serial.print("Toggling closed, chip ");
+                  Serial.print(Cnums[switch_index][port_index]);
+                  Serial.print(" pin ");
+                  Serial.println(clsPnums[switch_index][port_index]);
+                  activeCnum = Cnums[switch_index][port_index];
+                  pulse_start = millis();
+                  pulseDuration = clsPulseDuration;
+                } else {
+                  Serial.println("! Please specify opn or cls (for open or close)");
+                }
               } else {
-                Serial.println("! Please specify opn or cls (for open or close)");
+                Serial.println("! Port specified not in 1..6");
               }
             } else {
-              Serial.println("! Port specified not in 1..6");
-            }
-          } else {
-          Serial.println("! Port specified is NaN");
+            Serial.println("! Port specified is NaN");
+          }
+        } else {
+          Serial.println("! Switch specified not in 1..3");        
         }
       } else {
-        Serial.println("! Switch specified not in 1..3");        
+        Serial.println("! Switch number specified is NaN");
       }
+    
     } else {
-      Serial.println("! Switch number specified is NaN");
+      Serial.println("! Unknown command");
     }
-  
   } else {
-    Serial.println("! Unknown command");
+    Serial.println("! Control board busy pulsing");
   }
 }
 
@@ -219,22 +242,18 @@ void setup() {
   for( i = 0;i < 9;i++){
     Wire.beginTransmission(chipAddr[i]);
     error = Wire.endTransmission();
-    Serial.print("  \n");
-    Serial.println(chipAddr[i],HEX);
-    
-    Serial.print("error is ");
-    Serial.print(error);
     if (error == 0)
     {
-      Serial.print("   I2C device found at address");
-     
+      Serial.print("I2C device found at address ");
+      Serial.println(chipAddr[i],HEX);
     }
-    else if (error == 4){
-      Serial.print("unknown error at address"); 
+    else {
+      Serial.print("! Error ");
+      Serial.print(error);
+      Serial.print(" at address "); 
+      Serial.println(chipAddr[i],HEX);
     }
-      
   }
-  Serial.println();
 }
 
 void loop() {
@@ -244,6 +263,29 @@ void loop() {
     messageParse(instring);
     instring = "";
     messageDone = false;
+  }
+
+  if (activeCnum >= 0) {
+    if (millis() - pulse_start >= pulseDuration) {
+      offOutput(activeCnum);
+      activeCnum = -1;
+      Serial.println("Done.");
+    }
+  }
+
+  if (streamOn) {
+    tick_count++;
+    if (tick_count % ticks_per_read == 0) {
+      Vout_meas = getVoltage();
+      Serial.print("V: ");
+      Serial.print(Vout_meas);
+      Serial.println(" V");
+      current = getCurrent();
+      Serial.print("I: ");
+      Serial.print(current);
+      Serial.println(" mA");
+      tick_count = 0;
+    }
   }
 
 }
